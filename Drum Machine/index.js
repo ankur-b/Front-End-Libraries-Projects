23c08875bf6d028c4e076859ef1bfd36c431a49f
ankur-b <barveankur29@outlook.com>
2018-08-07 22:42:28 +0530

    add Drum Machine

---
+++ Drum Machine/index.js
@@ -0,0 +1,28 @@
+function playsound1(){
+	var sample =  document.getElementById('clap');
+	sample.play();
+}
+function playsound2(){
+	var sample =  document.getElementById('crash');
+	sample.play();
+}
+function playsound3(){
+	var sample =  document.getElementById('hat');
+	sample.play();
+}
+function playsound4(){
+	var sample =  document.getElementById('kick');
+	sample.play();
+}
+function playsound5(){
+	var sample =  document.getElementById('snare');
+	sample.play();
+}
+function playsound6(){
+	var sample =  document.getElementById('tom');
+	sample.play();
+}
+function playsound7(){
+	var sample =  document.getElementById('virus');
+	sample.play();
+}
